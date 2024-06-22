5ac6d7c022d050d27e27f92d2952f6eded2b033d
Bonnie Huang <molk3206@gmail.com>
2024-06-22 18:52:36 +0800

    Initial commit

---
+++ Debug/FreeRTOS/event_groups.d
@@ -0,0 +1,27 @@
+FreeRTOS/event_groups.o: ../FreeRTOS/event_groups.c \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/FreeRTOS.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/FreeRTOSConfig.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/projdefs.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/portable.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/deprecated_definitions.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/mpu_wrappers.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/task.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/list.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/timers.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/task.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/event_groups.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/timers.h
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/FreeRTOS.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/FreeRTOSConfig.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/projdefs.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/portable.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/deprecated_definitions.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/mpu_wrappers.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/task.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/list.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/timers.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/task.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/event_groups.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/timers.h:
---
+++ Debug/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,19 @@
+FreeRTOS/portable/MemMang/heap_4.o: ../FreeRTOS/portable/MemMang/heap_4.c \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/FreeRTOS.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/FreeRTOSConfig.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/projdefs.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/portable.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/deprecated_definitions.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/mpu_wrappers.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/task.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/list.h
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/FreeRTOS.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/FreeRTOSConfig.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/projdefs.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/portable.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/deprecated_definitions.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/mpu_wrappers.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/task.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_0/FreeRTOS/include/list.h:
